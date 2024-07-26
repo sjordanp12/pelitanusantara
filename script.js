a1c6445319504bfef1c067e852e8fd90a51fd316
Septian Jordan Purba <142944749+sjordanp12@users.noreply.github.com>
2024-07-26 14:47:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,31 +32,64 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
   });
 });
 
-document.getElementById('myFormm').addEventListener('submit', function (event) {
-  event.preventDefault(); // Menghentikan pengiriman form standar
+const form = document.getElementById('myFormm');
+const submitButton = document.getElementById('submit');
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxsrKZiXTsZpM8Q0BIVgfsuoRMlX7_GircBkWvqGKoCHHdbjiUASd7LNcx1t8YskUf0ow/exec';
 
-  // Mengambil nilai dari input nama dan email
-  var email = document.getElementById('exampleInputEmail1').value.trim();
-  var comment = document.getElementById('floatingTextarea2').value.trim();
-
-  // Validasi input
-  if (email === '' || comment === '') {
-    // Menampilkan pesan SweetAlert jika ada input yang kosong
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Mohon lengkapi semua kolom!',
-    });
-  } else {
-    // Menampilkan pesan SweetAlert jika input valid
-    Swal.fire({
-      icon: 'success',
-      title: 'Terima kasih!',
-      text: 'Form berhasil dikirim.',
-    });
-
-    // Mengosongkan nilai input setelah submit berhasil
-    document.getElementById('exampleInputEmail1').value = '';
-    document.getElementById('floatingTextarea2').value = '';
-  }
+myFormm.addEventListener('submit', (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  let requestBody = new FormData(myFormm);
+  fetch(scriptURL, { method: 'POST', body: requestBody });
+  // .then((response) => {
+  //   Swal.fire({
+  //     icon: 'success',
+  //     title: 'Terima kasih!',
+  //     text: 'Form berhasil dikirim.',
+  //   });
+  //   // 'Success!', response;
+  //   submitButton.disabled = false;
+  // })
+  // .catch((error) => {
+  //   alert('Error!', error.message);
+  //   submitButton.disabled = false;
+  // });
+  Swal.fire({
+    icon: 'success',
+    title: 'Terima kasih!',
+    text: 'Masukan anda berhasil dikirim.',
+  });
+  submitButton.disabled = false;
+  // Mengosongkan nilai input setelah submit berhasil
+  document.getElementById('Email').value = '';
+  document.getElementById('Message').value = '';
 });
+
+// document.getElementById('myFormm').addEventListener('submit', function (event) {
+//   event.preventDefault(); // Menghentikan pengiriman form standar
+
+//   // Mengambil nilai dari input nama dan email
+//   var email = document.getElementById('Email');
+//   var comment = document.getElementById('Message');
+
+//   // Validasi input
+//   if (email === '' || comment === '') {
+//     // Menampilkan pesan SweetAlert jika ada input yang kosong
+//     Swal.fire({
+//       icon: 'error',
+//       title: 'Oops...',
+//       text: 'Mohon lengkapi semua kolom!',
+//     });
+//   } else {
+//     // Menampilkan pesan SweetAlert jika input valid
+// Swal.fire({
+//   icon: 'success',
+//   title: 'Terima kasih!',
+//   text: 'Form berhasil dikirim.',
+// });
+
+//     // Mengosongkan nilai input setelah submit berhasil
+//     document.getElementById('Email').value = '';
+//     document.getElementById('Message').value = '';
+//   }
+// });
