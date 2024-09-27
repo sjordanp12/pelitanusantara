ed0d6b88657c713f777db8d84fc1f60c3c84d761
Septian Jordan Purba <142944749+sjordanp12@users.noreply.github.com>
2024-09-28 06:38:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.querySelector('.btn').addEventListener('click', function () {
-  swal('Terimakasih', ' pesan anda sudah dikirim', 'success');
+// document.querySelector('.btn').addEventListener('click', function () {
+//   swal('Terimakasih', ' pesan anda sudah dikirim', 'success');
+// });
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Mencegah pengiriman form
+
+  let nameInput = document.getElementById('email').value;
+  let commentInput = document.getElementById('comment').value;
+
+  if (nameInput && commentInput === '') {
+    Swal.fire({
+      title: 'Oops...',
+      text: 'Form tidak boleh kosong!',
+      icon: 'warning',
+      confirmButtonText: 'OK',
+    });
+  } else {
+    // Jika form diisi, lakukan tindakan lain (misalnya kirim data)
+    Swal.fire({
+      title: 'Berhasil!',
+      text: 'Form telah dikirim!',
+      icon: 'success',
+      confirmButtonText: 'OK',
+    });
+    // Di sini Anda bisa menambahkan logika untuk mengirim data
+  }
 });
